5ba7ef6c2c71f0c55b6ae2abc6f182778167fc1a
412330-Fenoglio-Santino <412330@tecnicatura.frc.utn.edu.ar>
2026-05-20 21:32:48 -0300

    Feat: Infraestructura base con Docker Compose, PostGIS y dependencias backend finalizadas

---
+++ motovias-backend/src/main/java/_0/motovias_backend/MotoviasBackendApplication.java
@@ -0,0 +1,13 @@
+package _0.motovias_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoviasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoviasBackendApplication.class, args);
+	}
+
+}
---
+++ motovias-backend/src/test/java/_0/motovias_backend/MotoviasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package _0.motovias_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoviasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
